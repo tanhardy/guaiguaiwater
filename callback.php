0624bb67b1d9567133bfda2f94933f383d285296
jimmy8646 <yujiechueh@gmail.com>
2016-11-01 17:40:17 +0800

    shit

--- callback.php
+++ callback.php
@@ -1,7 +1,7 @@
 <?php
 $jsonstring = file_get_contents('php://input');
 $jsonobject = json_decode($jsonstring);
-file_put_contents("debug.txt", $jsonobject,FILE_APPEND);
+file_put_contents("debug.txt", $jsonstring,FILE_APPEND);
 // require './vendor/autoload.php';
 //
 // $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('8WMjhheTQjsHniTtkyaZTDZi7+ntxMbY7rClcesJrjEhoSjwJsW50KlkPmjiR/SuE4vrkQZgiN/aix518Xk+Pfkn9ZBj6UdrUanpBi+99le6vNd65efQY731EJYFqe77lZud8NguNJcHgYFf7JCfvQdB04t89/1O/w1cDnyilFU=');
