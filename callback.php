490a27a3866064cf68018aaa4742e640a1768c10
jimmy8646 <yujiechueh@gmail.com>
2016-11-01 17:56:57 +0800

    echo

--- callback.php
+++ callback.php
@@ -7,4 +7,5 @@ require './vendor/autoload.php';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('8WMjhheTQjsHniTtkyaZTDZi7+ntxMbY7rClcesJrjEhoSjwJsW50KlkPmjiR/SuE4vrkQZgiN/aix518Xk+Pfkn9ZBj6UdrUanpBi+99le6vNd65efQY731EJYFqe77lZud8NguNJcHgYFf7JCfvQdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'ee39d46841657ce270c34c1f9a084c48']);
 $replytoken=$jsonobject->events[0]->replyToken;
-$response = $bot->replyText($replytoken, 'hello!');
+$echotext=$jsonobject->events[0]->message->text;
+$response = $bot->replyText($replytoken, $echotext);
