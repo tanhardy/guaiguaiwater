1441d019b9789439a01b3c291a5ba398d95e33e1
jimmy8646 <yujiechueh@gmail.com>
2016-11-01 21:49:36 +0800

    test

--- callback.php
+++ callback.php
@@ -11,11 +11,11 @@ $replytoken = $jsonobject->events[0]->replyToken;
 $get_text = $jsonobject->events[0]->message->text;
 
 if ($get_text == '機器人回報!') {
-    $response_text = file_get_contents('report.txt');
-    file_put_contents('report.txt', '');
-} elseif (preg_match('/^\d\d\s/', $get_text,$matches)) {  //頭匹配符ex:兩個數字開頭加上一個空白
+    $str = file_get_contents('report.txt');
+    $textarr=explode('\n',$str);
+    $response = $bot->replyText($replytoken, $textarr);
+    // file_put_contents('report.txt', '');
+} elseif (preg_match('/^[0-9]{0,2}\s.*/', $get_text,$matches)) {  //頭匹配符ex:兩個數字開頭加上一個空白
     file_put_contents('report.txt', $get_text, FILE_APPEND);
     file_put_contents('report.txt', "\n", FILE_APPEND);
 }
-
-$response = $bot->replyText($replytoken, $response_text);
