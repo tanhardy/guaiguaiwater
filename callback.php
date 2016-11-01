ac6ef1cca7aad068fd1905bf6b1640c45b9f4034
jimmy8646 <yujiechueh@gmail.com>
2016-11-01 17:31:24 +0800

    test

--- callback.php
+++ callback.php
@@ -1,18 +1,9 @@
 <?php
+$jsonstring = file_get_contents('php://input');
+$jsonobject = json_decode($jsonstring);
 
-// $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('8WMjhheTQjsHniTtkyaZTDZi7+ntxMbY7rClcesJrjEhoSjwJsW50KlkPmjiR/SuE4vrkQZgiN/aix518Xk+Pfkn9ZBj6UdrUanpBi+99le6vNd65efQY731EJYFqe77lZud8NguNJcHgYFf7JCfvQdB04t89/1O/w1cDnyilFU=');
-// $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'ee39d46841657ce270c34c1f9a084c48']);
-// $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-// $response = $bot->replyMessage('<reply token>', $textMessageBuilder);
-// if ($response->isSucceeded()) {
-//     echo 'Succeeded!';
+require './vendor/autoload.php';
 
-//     return;
-// }
-
-// // Failed
-// echo $response->getHTTPStatus.' '.$response->getRawBody();
-
-$content = "this is just a test.";
-
-file_put_contents("debug.txt", $content,FILE_APPEND);
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('8WMjhheTQjsHniTtkyaZTDZi7+ntxMbY7rClcesJrjEhoSjwJsW50KlkPmjiR/SuE4vrkQZgiN/aix518Xk+Pfkn9ZBj6UdrUanpBi+99le6vNd65efQY731EJYFqe77lZud8NguNJcHgYFf7JCfvQdB04t89/1O/w1cDnyilFU=');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'ee39d46841657ce270c34c1f9a084c48']);
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
