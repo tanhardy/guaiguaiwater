05ed9d89b3e7e43ca1592ac24d5e6640741b1c06
jimmy8646 <yujiechueh@gmail.com>
2016-11-01 19:51:56 +0800

    add case

--- callback.php
+++ callback.php
@@ -7,5 +7,15 @@ require './vendor/autoload.php';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('8WMjhheTQjsHniTtkyaZTDZi7+ntxMbY7rClcesJrjEhoSjwJsW50KlkPmjiR/SuE4vrkQZgiN/aix518Xk+Pfkn9ZBj6UdrUanpBi+99le6vNd65efQY731EJYFqe77lZud8NguNJcHgYFf7JCfvQdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'ee39d46841657ce270c34c1f9a084c48']);
 $replytoken=$jsonobject->events[0]->replyToken;
-$echotext=$jsonobject->events[0]->message->text;
-$response = $bot->replyText($replytoken, $echotext);
+$get_text=$jsonobject->events[0]->message->text;
+
+switch ($get_text) {
+    case '班頭清查人數開始':
+        $response_text="向前一步走！向左轉";
+        break;
+
+    default:
+        $response_text=$get_text;
+        break;
+}
+$response = $bot->replyText($replytoken, $response_text);
