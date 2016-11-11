40193e87666711601ebc3e02b428652e84fc163c
jimmy8646 <yujiechueh@gmail.com>
2016-11-11 23:48:18 +0800

    change secret code

--- callback.php
+++ callback.php
@@ -4,9 +4,10 @@ $jsonstring = file_get_contents('php://input');
 $jsonobject = json_decode($jsonstring);
 file_put_contents('debug.txt', $jsonstring, FILE_APPEND);
 require './vendor/autoload.php';
-
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('8WMjhheTQjsHniTtkyaZTDZi7+ntxMbY7rClcesJrjEhoSjwJsW50KlkPmjiR/SuE4vrkQZgiN/aix518Xk+Pfkn9ZBj6UdrUanpBi+99le6vNd65efQY731EJYFqe77lZud8NguNJcHgYFf7JCfvQdB04t89/1O/w1cDnyilFU=');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'ee39d46841657ce270c34c1f9a084c48']);
+$token = getenv('token');
+$channelSecret = getenv('channelSecret');
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient("$token");
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => "$channelSecret"]);
 $replytoken = $jsonobject->events[0]->replyToken;
 $get_text = $jsonobject->events[0]->message->text;
 
